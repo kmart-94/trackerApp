87018182649bed718c950d70c242c121649e6ee8
Kristian Martinez <kmartman94@gmail.com>
2020-07-20 14:50:42 -0700

    initial commit for track server

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+//body parser should be used before authRoutes to ensure that
+//all json info is parsed first and then run request handler
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = "mongodb+srv://admin:myp@ssword12@clustertracker.nry3n.mongodb.net/<dbname>?retryWrites=true&w=majority";
+//connects to mongoose using your uri and an options object with settings for avoiding errors later.
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+//code for detecting connection success
+mongoose.connection.on('connected', () => {
+  console.log("Connected to mongo instance!!!");
+});
+
+//code for detecting connection failure
+mongoose.connection.on('error', (err) => {
+  console.error('Error connectiong to mongo', err);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hi There!');
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
