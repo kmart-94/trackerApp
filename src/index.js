afffc84b951a9363e1dc302720a9ec352abcdf21
Kristian Martinez <kmartman94@gmail.com>
2020-07-20 19:50:59 -0700

    Add json web token creation and json web token confirmation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
-
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
@@ -31,8 +31,8 @@ mongoose.connection.on('error', (err) => {
   console.error('Error connectiong to mongo', err);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hi There!');
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
